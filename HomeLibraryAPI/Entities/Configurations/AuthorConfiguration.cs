ac52595e6ff8c4243b6f0a7f7a3ec734f9e6bfc3
Cezary Bros <cezary.bros@gmail.com>
2021-06-01 09:58:36 +0200

    Add models configuration

---
+++ HomeLibraryAPI/Entities/Configurations/AuthorConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Models;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Entities.Configurations
+{
+    public class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.FirstName)
+                .IsRequired();
+            builder.Property(a => a.LastName)
+                .IsRequired();
+        }
+    }
+}
