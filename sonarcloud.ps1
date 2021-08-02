34b07883969ea663ee1bebf3bcc97edf92948a16
Cezary Broś <cezary.bros@gmail.com>
2021-08-03 00:18:41 +0200

    Update pipeline

--- sonarcloud.ps1
+++ sonarcloud.ps1
@@ -2,6 +2,9 @@ param(
     [string] $sonarSecret
 )
 
+Install-package BuildUtils -Confirm:$false -Scope CurrentUser -Force
+Import-Module BuildUtils
+
 $runningDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 $testOutputDir = "$runningDirectory/TestResults"
