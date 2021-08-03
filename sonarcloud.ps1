72b2bab096a2dbe10b621dc650a37218ee907fb4
Cezary Broś <cezary.bros@gmail.com>
2021-08-03 08:47:16 +0200

    Update ps script

--- sonarcloud.ps1
+++ sonarcloud.ps1
@@ -2,9 +2,6 @@ param(
     [string] $sonarSecret
 )
 
-Install-package BuildUtils -Confirm:$false -Scope CurrentUser -Force
-Import-Module BuildUtils
-
 $runningDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 $testOutputDir = "$runningDirectory/TestResults"
@@ -15,12 +12,14 @@ if (Test-Path $testOutputDir)
     Remove-Item $testOutputDir -Recurse -Force
 }
 
-.\.sonar\scanner\dotnet-sonarscanner begin /k:"cbmlody_home-library-app" /o:"cbmlody" /d:sonar.login="$sonarSecret" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths=TestResults/*.trx /d:sonar.cs.opencover.reportsPaths=TestResults/*/coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs" dotnet restore .\HomeLibraryAPI\HomeLibraryAPI.sln
+.\.sonar\scanner\dotnet-sonarscanner begin /k:"cbmlody_home-library-app" /o:"cbmlody" /d:sonar.login="$sonarSecret" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.cs.vstest.reportsPaths=TestResults/*.trx /d:sonar.cs.opencover.reportsPaths=TestResults/*/coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs"
+Write-Host "Restoring dependencies..."
+dotnet restore .\HomeLibraryAPI\HomeLibraryAPI.sln
 
 Write-Host "Starting build..."
-dotnet build .\HomeLibraryAPI\HomeLibraryAPI.sln --configuration Release
+dotnet build .\HomeLibraryAPI\ --configuration Release
 
 Write-Host "Starting tests..."
-dotnet test ".\HomeLibraryAPI\HomeLibraryAPI.sln" --collect:"XPlat Code Coverage" -r .\TestResults --logger "trx;LogFileName=unittests.trx" --no-build --no-restore --configuration Release -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover
+dotnet test .\HomeLibraryAPI\HomeLibraryAPI.sln --collect:"XPlat Code Coverage" -r .\TestResults --logger "trx;LogFileName=unittests.trx" --no-build --no-restore --configuration Release -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencover
 
 .\.sonar\scanner\dotnet-sonarscanner end /d:sonar.login="$sonarSecret"
