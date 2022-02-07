78fc9b7f01dd0d5866dae00a451c81e6ee5cfb1a
Cezary Broś <cezary.bros@gmail.com>
2022-02-07 01:14:36 +0100

    Cleanup ps script

--- sonarcloud.ps1
+++ sonarcloud.ps1
@@ -2,9 +2,6 @@ param(
     [string] $sonarSecret
 )
 
-Install-package BuildUtils -Confirm:$false -Scope CurrentUser -Force
-Import-Module BuildUtils
-
 $runningDirectory = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 $testOutputDir = "$runningDirectory/TestResults"
