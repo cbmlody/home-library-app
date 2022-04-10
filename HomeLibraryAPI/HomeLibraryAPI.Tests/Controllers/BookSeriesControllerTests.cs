6ebe0a30fe8a2b78406b83f02df471af5553caa9
Cezary Broś <cezary.bros@gmail.com>
2022-04-10 23:31:07 +0200

    Add BooSeries Controller

---
+++ HomeLibraryAPI/HomeLibraryAPI.Tests/Controllers/BookSeriesControllerTests.cs
@@ -0,0 +1,109 @@
+﻿using AutoMapper;
+
+using FluentAssertions;
+
+using HomeLibraryAPI.Contracts;
+using HomeLibraryAPI.Controllers;
+using HomeLibraryAPI.EF.DTO;
+using HomeLibraryAPI.EF.Models;
+using HomeLibraryAPI.Tests.Helpers;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace HomeLibraryAPI.Tests.Controllers
+{
+    public class BookSeriesControllerTests
+    {
+        private readonly Mock<IRepositoryWrapper> _repository;
+        private readonly Mock<ILoggerManager> _logger;
+        private readonly IMapper _mapper;
+        private readonly BookSeriesController _controller;
+
+        private readonly List<BookSeries> _bookSeries = new()
+        {
+            new BookSeries { Id = new Guid("97d38b2a-5061-420c-b7a1-9d58073b142a"), Name = "First Book Series" },
+            new BookSeries { Id = new Guid("34993693-447d-4f69-82a2-baa254e1ab8f"), Name = "Second Book Series" },
+            new BookSeries { Id = new Guid("f2486e7b-c434-4378-99cf-6f94cad10168"), Name = "Last Book Series" }
+        };
+
+        public BookSeriesControllerTests()
+        {
+            _logger = new Mock<ILoggerManager>();
+            _repository = new Mock<IRepositoryWrapper>();
+            _mapper = TestHelper.InitializeMapper();
+            _controller = new BookSeriesController(_logger.Object, _repository.Object, _mapper);
+        }
+
+        [Fact]
+        public async Task GetAllBookSeries_WhenCalled_ReturnsOkResult()
+        {
+            // Arrange
+            _repository.Setup(e => e.BookSeries.GetAllAsync()).ReturnsAsync(_bookSeries);
+
+            // Act
+            var result = await _controller.GetAllBookSeries();
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public async Task GetAllBookSeries_WhenCalled_ReturnsAllItems()
+        {
+            // Arrange
+            _repository.Setup(e => e.BookSeries.GetAllAsync()).ReturnsAsync(_bookSeries);
+
+            // Act
+            var result = await _controller.GetAllBookSeries();
+            var bookSeriesResult = (result as OkObjectResult).Value;
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            _repository.Verify(m => m.BookSeries.GetAllAsync(), Times.Once());
+            bookSeriesResult.As<List<BookSeriesDto>>().Count.Should().Be(_bookSeries.Count);
+        }
+
+        [Fact]
+        public async Task GetBookSeriesById_WhenCalled_ReturnsOkResult()
+        {
+            // Arrange
+            Guid id = Guid.Empty;
+            _repository.Setup(e => e.BookSeries.GetByIdAsync(id)).ReturnsAsync(new BookSeries());
+
+            // Act
+            var result = await _controller.GetBookSeriesById(id);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public async Task GetBookSeriesById_WhenCalled_ReturnsSingleItem()
+        {
+            // Arrange
+            var expected = _bookSeries.Last();
+            var id = expected.Id;
+            _repository.Setup(e => e.BookSeries.GetByIdAsync(id)).ReturnsAsync(expected);
+
+            // Act
+            var result = await _controller.GetBookSeriesById(id);
+            var bookSeriesResult = (result as OkObjectResult).Value;
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            bookSeriesResult.Should().BeOfType<BookSeriesDto>();
+            bookSeriesResult.As<BookSeriesDto>().Id.Should().Be(expected.Id);
+            bookSeriesResult.As<BookSeriesDto>().Name.Should().Be(expected.Name);
+            _repository.Verify(e => e.BookSeries.GetByIdAsync(id), Times.Once());
+        }
+    }
+}
---
+++ HomeLibraryAPI/HomeLibraryAPI/Controllers/BookSeriesController.cs
@@ -0,0 +1,127 @@
+﻿using AutoMapper;
+
+using HomeLibraryAPI.Contracts;
+using HomeLibraryAPI.EF.DTO;
+using HomeLibraryAPI.EF.Models;
+using HomeLibraryAPI.EF.UpdateDTO;
+
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HomeLibraryAPI.Controllers
+{
+    [Route("api/bookseries")]
+    [ApiController]
+    public class BookSeriesController : ControllerBase
+    {
+        private readonly ILoggerManager _logger;
+        private readonly IRepositoryWrapper _repository;
+        private readonly IMapper _mapper;
+
+        public BookSeriesController(ILoggerManager logger, IRepositoryWrapper repository, IMapper mapper)
+        {
+            _logger = logger;
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBookSeries()
+        {
+            var bookSeries = await _repository.BookSeries.GetAllAsync();
+            _logger.LogInfo("Returnes all authors from db.");
+
+            var bookSeriesResult = _mapper.Map<IEnumerable<BookSeriesDto>>(bookSeries);
+            return Ok(bookSeriesResult);
+        }
+
+        [HttpGet("{id}", Name = "GetBookSeriesById")]
+        public async Task<IActionResult> GetBookSeriesById(Guid id)
+        {
+            var bookSerie = await _repository.BookSeries.GetByIdAsync(id);
+
+            if (bookSerie is null)
+            {
+                _logger.LogError($"Could not find book series with id:{id} in db");
+                return NotFound();
+            }
+
+            _logger.LogInfo($"Returned book series with id:{id} from db");
+            var bookSeriesResult = _mapper.Map<IEnumerable<BookSeriesDto>>(bookSerie);
+
+            return Ok(bookSeriesResult);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBookSeries(BookSeriesCreateUpdateDto bookSeries)
+        {
+            if (bookSeries is null)
+            {
+                _logger.LogError("BookSeries object sent from client is null");
+                return BadRequest("Book series object is null");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError("Invalid book series object sent from client");
+                return BadRequest("Invalid book series object");
+            }
+
+            var bookSeriesEntity = _mapper.Map<BookSeries>(bookSeries);
+            _repository.BookSeries.Create(bookSeriesEntity);
+            await _repository.SaveAsync();
+
+            var createdBookSeries = _mapper.Map<BookSeriesDto>(bookSeriesEntity);
+
+            return CreatedAtRoute("GetBookSeriesById", new { id = createdBookSeries.Id }, createdBookSeries);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBookSeries(Guid id, [FromBody] BookSeriesCreateUpdateDto bookSeries)
+        {
+            if (bookSeries is null)
+            {
+                _logger.LogError("Book series object sent from client is null");
+                return BadRequest("Book series object is null");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError("Invalid book series object sent from client");
+                return BadRequest("Invalid book series object sent from client");
+            }
+
+            var bookSeriesEntity = await _repository.BookSeries.GetByIdAsync(id);
+            if (bookSeriesEntity is null)
+            {
+                _logger.LogError($"Book series with id:{id} was not found in db");
+                return NotFound();
+            }
+
+            _mapper.Map(bookSeries, bookSeriesEntity);
+            _repository.BookSeries.Update(bookSeriesEntity);
+            await _repository.SaveAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBookSeries(Guid id)
+        {
+            var booKSeriesEntity = await _repository.BookSeries.GetByIdAsync(id);
+            if (booKSeriesEntity is null)
+            {
+                _logger.LogError($"Book series with id:{id} was not found in db");
+                return NotFound();
+            }
+
+            _repository.BookSeries.Delete(booKSeriesEntity);
+            await _repository.SaveAsync();
+
+            return NoContent();
+        }
+    }
+}
